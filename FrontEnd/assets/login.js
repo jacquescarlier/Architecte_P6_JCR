c6c8967e59bfb027932e1d127870a4bbf1e20b91
Jacques Carlier <jacques.carlier@gmail.com>
2023-05-22 09:53:42 +0200

    modification code and renaming of variable in login.js et .html

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,67 +1,67 @@
 // constante url login
 let urlLogin = "http://localhost:5678/api/users/login";
 //constante pour le message d'erreur
+const submitSeConnecter = document.getElementById("submit-connecter")
 const errorInformation = document.getElementById("errorInformation");
 //style reset for errorInformation
 errorInformation.style.background = "none";
 errorInformation.style.border = "none ";
+// focus in input email
+document.getElementById("emailUser").focus()
 //  -----------------------------------------------
 //  | added event listening on the connect button |
 //  -----------------------------------------------
-submit.addEventListener("click", (e) => {
+submitSeConnecter.addEventListener("click", (e) => {
     e.preventDefault();
     /*récupération de l'objet event et lui applique la méthode 'preventDefault'. Bloque l'action par défaut du bouton si l'évennement n'est pas explicitement géré */
-    const email = document.getElementById("emailUser").value;
-    const password = document.getElementById("password").value;
+    let email = document.getElementById("emailUser").value;
+    let password = document.getElementById("password").value;
 
     if (!email || !password) {
         console.log(email)
-        email.value = ' ';
-        password.value = '';
         document.getElementById("errorInformation").innerHTML =
-            "Entrer un identifiant ou un mot de passe valide"    
-        // change font & background color    
+            "Entrer un identifiant ou un mot de passe valide"
         errorInformation.style.background = "#F0CCD8";
         errorInformation.style.border = "solid 1px #FF0000";
         return;
     }
-//  ------------------
-//  | API connection |
-//  ------------------
-fetch(urlLogin, {
-        
-    method: "POST",
-    headers: {
-        accept: "application/json",
-        "Content-type": "application/json",
-    },
-    body: JSON.stringify({ email: email, password: password }),
+    //  ------------------
+    //  | API connection |
+    //  ------------------
+    fetch(urlLogin, {
+
+        method: "POST",
+        headers: {
+            accept: "application/json",
+            "Content-type": "application/json",
+        },
+        body: JSON.stringify({ email: email, password: password }),
     })
-    //  ---------------------------
-    //  | Connection status check |
-    //  ---------------------------
-    .then(function (authResponse) {
-        if (authResponse.status === 200) {
-            return authResponse.json();
-        } else {
-             errorInformation.innerHTML =
-                "Erreur dans l'identifiant ou le mot de passe";
+        //  ---------------------------
+        //  | Connection status check |
+        //  ---------------------------
+        .then(function (authResponse) {
+            if (authResponse.status === 200) {
+                return authResponse.json();
+            } else {
+                errorInformation.innerHTML =
+                    "Erreur dans l'identifiant ou le mot de passe";
                 email.value = ""
-                password.value= ""
+                password.value = ""
                 errorInformation.style.background = "#F0CCD8";
-                errorInformation.style.border = "solid 1px #FF0000";   
-            return Promise.reject();
-        }
-    })
-    //  -------------------------------- 
-    //  | save token in sessionStorage |
-    //  --------------------------------
-    .then(function (userInformation) {
-        //if (userInformation) {
+                errorInformation.style.border = "solid 1px #FF0000";
+                return Promise.reject();
+            }
+        })
+        //  -------------------------------- 
+        //  | save token in sessionStorage |
+        //  --------------------------------
+        .then(function (userInformation) {
+            //if (userInformation) {
             sessionStorage.setItem("userInformation", JSON.stringify(userInformation));
             sessionStorage.setItem("token", userInformation.token);
-            window.location.replace("./index.html");  
-        //}
-    })
-    .catch((error) => console.error(error));
+            window.location.replace("./index.html");
+            //}
+        })
+        .catch((error) => console.error(error));
 });
