09388d35b2c731bcab639656f2bfae1be45294cc
Jacques Carlier <jacques.carlier@gmail.com>
2023-05-25 10:09:54 +0200

    code reorganization

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -2,7 +2,9 @@
 //  |  declaration of constants and variables |
 //  -------------------------------------------
 
-//url to access the api database
+// API call
+
+// url to access the api database
 let url = "http://localhost:5678/api/works";
 // url for the category part
 let urlCategories = "http://localhost:5678/api/categories";
@@ -12,10 +14,10 @@ const gallery = document.querySelector(".gallery");
 const adminNav = document.getElementById("admin-nav");
 // "edit" buttons for the admin part
 const modalCallButtons = document.querySelectorAll(".modifier");
-//login or logout button depending on the token
+// login or logout button depending on the token
 const loginLogout = document.getElementById("login");
 
-/*tableau ['entitled','suffix for button ID' (btn-" "), 'category name']
+/* tableau ['entitled','suffix for button ID' (btn-" "), 'category name']
 tableau pour fonction "create button sans passer par l'API"
 Faute d'orthographe dans un nom de catégorie Hotel => Hôtel, désactivé pour l'instant
 let arrayCreateButton = [["Tous", "Tous", "Tous"],["Objets", "Objets", "Objets"], ["Appartements", "Appartements", "Appartements"], ["Hôtels & restaurants", "Hotels", "Hotels & restaurants"]];*/
@@ -51,21 +53,21 @@ async function createButtons() {
   //creation of buttons for each category
 
   for (let i = 0; i < category.length; i++) {
-    //variable pour la création d'un bouton
+    //variable for creating a button
     let newFilterButton = document.createElement("button");
-    //attribut type du bouton
+    //button type attribute
     newFilterButton.type = "button";
-    // attibut nom du bouton
+    // button name attribute
     newFilterButton.name = category[i].name;
-    // Texte entre les balises button
+    // button title
     newFilterButton.innerHTML = category[i].name;
-    // id du bouton "btn-"" + premier mot récupéré
+    // button id ("btn-" + first retrieved word)
     newFilterButton.id = "btn-" + category[i].name.split(" ")[0];
-    // class ajouté au bouton
+    // class add to button
     newFilterButton.className = "btn-filter";
     //variable portfolio
     let portfolio = document.getElementById("filterButton");
-    // ajout de "newFilterButton" à la div "#portfolio"
+    // adding buttons to " #portfolio "
     portfolio.appendChild(newFilterButton);
   }
 }
@@ -126,19 +128,21 @@ async function buildWorks() {
   async function createFilter() {
     let filterChoice = "";
     const btnFilter = document.querySelectorAll(".btn-filter");
+    // filter by category name
     btnFilter.forEach(function (btn) {
       btn.addEventListener("click", function (e) {
         if (e.target.name === "Tous") {
-          console.log("target-e", e.target);
+                                        console.log("target-e", e.target);
           filterChoice = works;
-          console.log("filterChoice", filterChoice);
+                                        console.log("filterChoice", filterChoice);
           applyFilter(filterChoice);
         } else {
           filterChoice = works.filter(
+           //  compares the name of the button to the categories of the "works" array object
             (obj) => obj.category.name === e.target.name
           );
-          console.log("target-e", e.target);
-          console.log("filterChoice", filterChoice);
+                                        console.log("target-e", e.target);
+                                        console.log("filterChoice", filterChoice);
           applyFilter(filterChoice);
         }
       });
@@ -175,18 +179,17 @@ async function buildWorks() {
   let controlToken = sessionStorage.getItem("token");
   console.log("token", controlToken);
   // Modifies elements when switching to "edit mode"
-  controlToken === null
-    ? (adminNav.style.display = "none")
+  controlToken === null ? (adminNav.style.display = "none")
     : (adminNav.style.display = "flex");
-  controlToken === null
-    ? (document.getElementById("login").innerHTML = "login")
+
+  controlToken === null ? (document.getElementById("login").innerHTML = "login")
     : (document.getElementById("login").innerHTML = "logout");
-  controlToken === null
-    ? (header.style.marginTop = "50px")
+
+  controlToken === null ? (header.style.marginTop = "50px")
     : (header.style.marginTop = "38px");
+
   modalCallButtons.forEach(function (item) {
-    controlToken === null
-      ? (item.style.display = "none")
+    controlToken === null ? (item.style.display = "none")
       : (item.style.display = "flex");
   });
 
@@ -195,9 +198,7 @@ async function buildWorks() {
   //  ---------------------------------------------------------
 
   // add event listening on the  "login/logout" button
-  loginLogout.addEventListener("click", function () {
-    logInLogOut();
-  });
+  loginLogout.addEventListener("click", function () { logInLogOut(); });
 
   // redirect function to a page according to the token
   function logInLogOut() {
@@ -222,28 +223,38 @@ async function buildWorks() {
   //  | constantes for modal |
   //  ------------------------
 
-  // gallery modal
+/**** gallery modal ****/
 
+  // general container & gallery
   const modalContainer = document.querySelector(".modal-container");
   const modalGallery = document.querySelector(".modal-galerie");
+  //all elements that can close or open the modal
   const modalTrigger = document.querySelectorAll(".modal-trigger");
+  // button to open 2nd modal
   const buttonAddPhotos = document.getElementById("button-add-modal");
+  // gallery container
   const galleryOfModal = document.querySelector(".gallery-modal");
 
-  //modal add photo
+/**** modal add photo ****/
 
   const modal2Container = document.querySelector(".modal2-container");
+  //all elements that can close or open the modal
   const modalTrigger2 = document.querySelectorAll(".modal-trigger2");
+  // general container to add a photo
   const addPhotos = document.querySelector(".add-photo");
-  const buttonValidPhoto = document.getElementById("button-add-photo");
+  //validate button
+  const buttonValidatePhoto = document.getElementById("button-validate-photo");
+  // messages for image size or error when selecting
   const infoFile = document.querySelector(".info-file");
+  // arrow previous
+  const previousArrow = document.getElementById("previous-arrow");
 
   //  ---------------------
   //  | modal 1 - gallery |
   //  ---------------------
-  //  |  Create gallery   |
-  //  ---------------------
-
+  
+  /**** Create gallery  ****/   
+  
   for (const project of works) {
     let figure = document.createElement("figure");
     let img = document.createElement("img");
@@ -259,71 +270,54 @@ async function buildWorks() {
     galleryOfModal.appendChild(figure);
   }
   //display modal
-  modalContainer.className === "modal-container active"
-    ? (modalgallery.style.display = "flex")
+  modalContainer.className === "modal-container active" ? (modalgallery.style.display = "flex")
     : (modalGallery.style.display = "none");
   //list of  elements listened to under the class "trigger"
-  modalTrigger.forEach((trigger) =>
-    trigger.addEventListener("click", toggleModal)
-  );
+  modalTrigger.forEach((trigger) => trigger.addEventListener("click", toggleModal));
   // change the name of the class using toggle
 
   function toggleModal() {
     modalContainer.classList.toggle("active");
     // makes the modal appears or disappear depending on the class
-    modalContainer.className === "modal-container active"
-      ? (modalGallery.style.display = "flex")
-      : (modalGallery.style.display = "none");
+    modalContainer.className === "modal-container active" ? (modalGallery.style.display = "flex") : modalGallery.style.display = "none";
   }
 
   //close the modal by clicking outside 
 
-  modalContainer.addEventListener("click", (e)=> {  toggleModal();}) ;
-  modalContainer.children[1].addEventListener('click', function (e) {
-    e.stopPropagation();
-})
+  modalContainer.addEventListener("click", (e) => { toggleModal(); });
+  modalContainer.children[1].addEventListener('click', function (e) { e.stopPropagation();});
 
-modal2Container.addEventListener("click", (e)=> {  toggleModal2();}) ;
-modal2Container.children[1].addEventListener('click', function (e) {
-  e.stopPropagation();
-})
+  modal2Container.addEventListener("click", (e) => { toggleModal2(); });
+  modal2Container.children[1].addEventListener('click', function (e) {e.stopPropagation();});
 
   //  ---------------------
   //  | modal - add photo |
   //  ---------------------
 
   //for close modal
-  modalTrigger2.forEach((trigger) =>
-    trigger.addEventListener("click", toggleModal2)
-  );
+  modalTrigger2.forEach((trigger) => trigger.addEventListener("click", toggleModal2));
 
   // conditions of appearance of the modal
   function toggleModal2() {
     modal2Container.classList.toggle("active");
-    
-    modalContainer.className === "modal-container active"
-      ? (modalGallery.style.display = "flex")
+
+    modalContainer.className === "modal-container active" ? (modalGallery.style.display = "flex")
       : (modalGallery.style.display = "none");
-    
-    modal2Container.className === "modal2-container active"
-      ? (addPhotos.style.display = "flex")
+
+    modal2Container.className === "modal2-container active" ? (addPhotos.style.display = "flex")
       : (addPhotos.style.display = "none");
-    
-    modal2Container.className === "modal2-container active"
-      ? (modalGallery.style.display = "none")
+
+    modal2Container.className === "modal2-container active" ? (modalGallery.style.display = "none")
       : (modalGallery.style.display = "flex");
   }
 
   //toogle funtion to manage the appearance of the modal
-  buttonAddPhotos.addEventListener("click", function () {
-    toggleModal();
-    toggleModal2();
-  });
+  buttonAddPhotos.addEventListener("click", function () { toggleModal(); toggleModal2();});
   //  ------------------------------------
   //  | modal - add photo Arrow previous |
   //  ------------------------------------
 
-  const previousArrow = document.getElementById("previous-arrow");
+  
 
   previousArrow.addEventListener("click", function () {
     toggleModal2();
@@ -338,31 +332,27 @@ modal2Container.children[1].addEventListener('click', function (e) {
   const containerAddPhoto = document.querySelector(".container-add-photo");
   infoFile.innerHTML = "jpg png : 4 mo max";
   inputFile.addEventListener("click", function (e) {
+    // modify the class of infoFile
     infoFile.classList = "info-file";
-
-  inputFile.addEventListener("change", function (e) {
-    document.querySelector(".info-file").innerHTML = "jpg png : 4 mo max";
-    const typeImg = inputFile.value.split(".").reverse()[0];
-      if (
-        typeImg === "png" ||
-        typeImg === "PNG" ||
-        typeImg === "jpg" ||
-        typeImg === "jpeg"
-      ) {
-        console.log("img Ok");
-        buttonValidPhoto.style.background = "#1D6154";
+    inputFile.addEventListener("change", function (e) {
+      document.querySelector(".info-file").innerHTML = "jpg png : 4 mo max";
+      const typeImg = inputFile.value.split(".").reverse()[0];
+      // file type check
+      if (typeImg === "png" || typeImg === "PNG" || typeImg === "jpg" || typeImg === "jpeg" ) {
+                                      console.log("img Ok");
+        // change the background color of the "validate" button
+        buttonValidatePhoto.style.background = "#1D6154";
       } else {
-        console.log("img not ok");
+                                      console.log("img not ok");
+        // change the text and modify the class of infofile
         infoFile.innerHTML = "fichier jpg ou png obligatoire";
         infoFile.classList = "message-info-file";
-
         return;
       }
-      //e.preventDefault();
+      
       image = e.target.files[0].name;
-
-      console.log("type =>", typeImg);
-      console.log("fichier =>", image);
+                                      console.log("type =>", typeImg);
+                                      console.log("fichier =>", image);
       let img = document.createElement("img");
       img.src = image;
       containerAddPhoto.appendChild(img);
