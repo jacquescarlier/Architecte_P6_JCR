137c2171403c591651be4a1bf0f7c5650053b8be
Jacques Carlier <jacques.carlier@gmail.com>
2023-06-06 09:13:40 +0200

    js code optimization

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -15,7 +15,6 @@ const loginLogout = document.getElementById("login");
 // ---------------------------------------------
 // | Check the connection with the API "works" |
 // ---------------------------------------------
-
 async function getWorks(url) {
   const response = await fetch(url);
   if (response.ok) {
@@ -24,20 +23,17 @@ async function getWorks(url) {
     return Promise.reject(`Erreur HTTP fetch 1 => ${response.status}`);
   }
 }
-
 //  ---------------------------------------------------------
 //  |   data recovery through the api url for categories    |
 //  ---------------------------------------------------------
-
 async function getCategories(urlCategories) {
   const response = await fetch(urlCategories);
   if (response.ok) return await response.json();
 }
-
 //  ------------------------
 //  | CategoryId for modal |
 //  ------------------------
-async function ForCategory() {
+async function forCategory() {
   let category = await getCategories(urlCategories);
   function categoryIdForModal() {
     const select = document.querySelector("#category");
@@ -49,11 +45,9 @@ async function ForCategory() {
     }
   }
   categoryIdForModal();
-
   //  ---------------------------------------------
   //  | Create buttons dynamically whith the API  |
   //  ---------------------------------------------
-
   async function createButtons() {
     for (let i = 0; i < category.length; i++) {
       let newFilterButton = document.createElement("button");
@@ -68,15 +62,13 @@ async function ForCategory() {
   }
   createButtons();
 }
-ForCategory();
+forCategory();
 //  --------------------------------------------
 //  |    Creation of the gallery & the filter  |
 //  --------------------------------------------
-
 async function buildWorks() {
   // array constant
   let works = await getWorks(url);
-
   // -------------------------
   // | job creation function |
   // -------------------------
@@ -87,7 +79,7 @@ async function buildWorks() {
     figure.className = "figure-gallery";
     let img = document.createElement("img");
     img.src = project.img;
-    img.alt = project.caption;
+    img.alt = project.alt;
     figure.appendChild(img);
     let figcaption = document.createElement("figcaption");
     figcaption.innerHTML = project.caption;
@@ -112,16 +104,15 @@ async function buildWorks() {
         "id": work.id,
         "img": work.imageUrl,
         "caption": work.title,
+        "alt": work.title
       }
       addNewProject(project, container, false);
     }
   }
   createWork(gallery);
-
   //  ----------------------------
   //  | filter creation function |
   //  ----------------------------
-
   async function createFilter() {
     let filterChoice = "";
     const btnFilter = document.querySelectorAll(".btn-filter");
@@ -140,11 +131,9 @@ async function buildWorks() {
     });
   }
   createFilter();
-
   // ------------------
   // |  Apply filter  |
   // ------------------
-
   function applyFilter(filterChoice) {
     gallery.innerHTML = "";
     for (const filter of filterChoice) {
@@ -158,11 +147,9 @@ async function buildWorks() {
       gallery.appendChild(figure);
     }
   }
-
   //  ---------------------------------------------
   //  | setting up element according to the token |
   //  ---------------------------------------------
-
   const btnsFilter = document.querySelector(".btns-filter");
   const mesProjetsMarginH2 = document.querySelector(".mes-projets");
 
@@ -189,16 +176,13 @@ async function buildWorks() {
     controlToken === null
       ? (item.style.display = "none") : (item.style.display = "flex");
   });
-
   //  ---------------------------------------------------------
   //  | Management of the action of the "login/logout" button |
   //  ---------------------------------------------------------
-
   // add event listening on the  "login/logout" button
   loginLogout.addEventListener("click", function () {
     logInLogOut();
   });
-
   // redirect function to a page according to the token
   function logInLogOut() {
     if (controlToken === null) {
@@ -208,17 +192,14 @@ async function buildWorks() {
       window.location.replace("./index.html");
     }
   }
-
   //  -----------------------
   //  | parts of the modals |
   //  -----------------------
-
   // general container & gallery
   const modalContainer = document.querySelector(".modal-container");
   const modalGallery = document.querySelector(".modal-photo-gallery");
   const modal2Container = document.querySelector(".modal2-container");
-  const addPhotos = document.querySelector(".add-photo");
-
+  const addPhotos = document.querySelector(".add-photo")
   /**** gallery modal ****/
   //all elements that can close or open the modal
   const modalTrigger = document.querySelectorAll(".modal-trigger");
@@ -226,7 +207,6 @@ async function buildWorks() {
   const buttonAddPhotos = document.getElementById("button-add-modal");
   // gallery container
   const galleryOfModal = document.querySelector(".gallery-modal");
-
   /**** modal add photo ****/
   //all elements that can close or open the modal
   const modalTrigger2 = document.querySelectorAll(".modal-trigger2");
@@ -237,29 +217,22 @@ async function buildWorks() {
   // arrow previous
   const previousArrow = document.getElementById("previous-arrow");
   // selected image in the modal added a photo
-
   //  ---------------------
   //  | modal 1 - gallery |
   //  ---------------------
-
   /**** Create the gallery for modal ****/
   async function createWorksForGallery() {
     for (const work of works) {
       let project = { 
         "id": work.id,
         "img": work.imageUrl,
-        "caption": "éditer"
+        "caption": "éditer",
+        "alt": work.title
       }
-      addNewProject(project, galleryOfModal, true)
-
-      // arrow hover
-    /*  img.addEventListener("mouseover", function (e) { arrowGallery.style.display = "flex"; });
-      img.addEventListener("mouseout", function (e) { arrowGallery.style.display = "none"; });
-      arrowGallery.addEventListener("mouseover", function () { arrowGallery.style.display = "flex"; })*/
+      addNewProject(project, galleryOfModal, true);
     }
   }
   createWorksForGallery();
-
   /****  containers for adding photos ****/
   const containerAddPhoto = document.querySelector(".container-add-photo");
   const containerAddPhoto2 = document.querySelector(".container-add-photo2");
@@ -276,9 +249,7 @@ async function buildWorks() {
     modalContainer.className === "modal-container active"
       ? (modalGallery.style.display = "flex") : (modalGallery.style.display = "none");
   }
-
   /**** delete image  ****/
-
   let trashButton = document.querySelectorAll(".fa-trash-can");
   let alertModalGallery = document.querySelector(".alert-modal");
 
@@ -312,12 +283,9 @@ async function buildWorks() {
     })
   );
 
-  //how the modal close
-
+  /**** how the modal close ****/
   modalGallery.addEventListener("click", function () { alertModalGallery.style.display = "none"; });
-
   alertModalGallery.innerHTML = " ";
-
   /**** close the modal by clicking outside ****/
   modalContainer.addEventListener("click", (e) => { toggleModal(); });
 
@@ -326,11 +294,9 @@ async function buildWorks() {
   modal2Container.addEventListener("click", (e) => { toggleModal2(); });
 
   modal2Container.children[1].addEventListener("click", function (e) { e.stopPropagation(); });
-
   //  ---------------------
   //  | modal - add photo |
   //  ---------------------
-
   //for close modal
   modalTrigger2.forEach((trigger) =>
     trigger.addEventListener("click", toggleModal2)
@@ -360,13 +326,11 @@ async function buildWorks() {
     buttonValidatePhoto.style.background = "#A7A7A7";
     buttonValidatePhoto.style.cursor = "default";
   }
-
   // toogle funtion to manage the appearance of the modal
   buttonAddPhotos.addEventListener("click", function () {
     toggleModal();
     toggleModal2();
   });
-
   /****modal - add photo Arrow previous ****/
   previousArrow.addEventListener("click", function () {
     modalContainer.classList.toggle("active");
@@ -374,22 +338,18 @@ async function buildWorks() {
     //const imageSelected = document.getElementById("image-selected");
     toggleModal2();
   });
-
   /**** add photo  input file part  ****/
-
   const fileUploadInput = document.querySelector("#my-file");
   infoFile.innerHTML = "jpg png : 4 mo max";
-  fileUploadInput.addEventListener("change", previewFile);
+  fileUploadInput.addEventListener("change", previewFileAndSendNewWork);
 
-  function previewFile() {
+  function previewFileAndSendNewWork() {
     const sizeFile = this.files[0].size;
     const imageUploaded = this.files[0];
-    imageName = imageUploaded.name;
-    splitImageUploaded = imageName.split(".");
-
+   // imageName = imageUploaded.name;
     // fichier avec . et jpeg ou jpg ou png en minuscule ou majuscule
     const fileExtensionRegex = /\.(jpe?g|png)$/i;
-    //.test renvoie true ou false
+    //.test renvoie true ou false par rapport au regex
     if (
       this.files.length === 0 || !fileExtensionRegex.test(this.files[0].name) || sizeFile > 4194304
     ) {//size file max  = 4* 1024 * 1024
@@ -421,11 +381,9 @@ async function buildWorks() {
       containerAddPhoto2.style.display = "flex";
       containerAddPhoto.style.display = "none";
     }
-
     /**** formData ****/
     const errorMessage = document.getElementById("errorMessage")
-    //comment désactiver le bouton
-    buttonValidatePhoto.disable = true;
+    // validation button
     buttonValidatePhoto.addEventListener("click", function (event) { sendNewWork() })
     let controleContenuInput = document.querySelectorAll(".controle-contenu");
     controleContenuInput.forEach((controle) =>
@@ -436,44 +394,30 @@ async function buildWorks() {
         }
       }));
 
+    // add new work  in gallery
     function addNewWorkGallery() {
-      workId = works.length + 1;
-      imgForWork = addedImage;
-      figcaptionNewWork = title.value;
-      addNewProject()
+      let project = { 
+        "id": works.length + 1,
+        "img": addedImage,
+        "caption": title.value,
+        "alt": title.value
+      }
+      addNewProject(project,gallery, false)
     }
-
-    // add new work
+    // add new work in modal
     function addNewWorkInModal() {
-      //figcaptionNewWork = "éditer";
-      //workId = works.length + 1;
-      //imgForWork = addedImage;
-      let idPhoto = works.length + 1;
-      let figure = document.createElement("figure");
-      figure.id = idPhoto;
-      figure.className = "figure-gallery";
-      let img = document.createElement("img");
-      img.src = addedImage;
-      figure.appendChild(img);
-      let figcaption = document.createElement("figcaption");
-      figcaption.innerHTML = "éditer";
-      figure.appendChild(figcaption);
-      let trashGallery = document.createElement("i");
-      trashGallery.className = "fa-solid fa-trash-can trash";
-      trashGallery.innerHTML = "";
-      figure.appendChild(trashGallery);
-      let arrowGallery = document.createElement("i");
-      arrowGallery.className = "fa-solid fa-arrows-up-down-left-right arrow";
-      arrowGallery.innerHTML = "";
-      figure.appendChild(arrowGallery);
-      galleryOfModal.appendChild(figure);
+      let project = { 
+        "id": works.length + 1,
+        "img": addedImage,
+        "caption": "éditer",
+      }
+      addNewProject(project, galleryOfModal, true)
     }
 
-    /**** Send new work in db****/
+    /**** Send new work in API db****/
     function sendNewWork(e) {
       const formData = new FormData();
       formData.append("image", imageUploaded);
-
       formData.append("title", title.value);
       formData.append("category", category.value);
 
